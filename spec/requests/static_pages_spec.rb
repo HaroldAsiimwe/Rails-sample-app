f74851d555cb5823cfb0ba94ce9987266d916317
Harold Asiimwe <harryasiimwe@gmail.com>
2013-09-09 14:00:02 +0300

    Finish static Pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+
+    it "sould have the right title" do 
+    	visit 'static_pages/home'
+    	page.should have_selector('title', :text => 'Rails sample Application | Home')
+    end
+  end
+
+  describe "About Page" do
+  	it "should have content 'About Us' " do
+  		visit '/static_pages/about'
+  		page.should have_content('About Us')
+  	end
+  end
+end
+
+
+
