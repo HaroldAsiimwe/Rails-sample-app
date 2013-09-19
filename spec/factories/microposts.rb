b767d64f831a667eaecd7b4d0e7b52e275bc2ed7
Harold Asiimwe <harryasiimwe@gmail.com>
2013-09-19 08:55:32 +0300

    Add user microposts

---
+++ spec/factories/microposts.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+	# Refer to the factories.rb for the :microposts definition
+
+  # factory :micropost do
+  #   content "MyString"
+  #   user_id 1
+  # end
+end
